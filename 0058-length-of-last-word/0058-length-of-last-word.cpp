ef770ebf964fb7a080358c3cb8af4268dca0895e
Kanishk.s <skanishk135@gmail.com>
2026-08-12 00:57:08 +0530

    Time: 0 ms (100%), Space: 9.1 MB (8.78%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i= s.length()-1;
+        int count = 0;
+     while(i>=0 && s[i]==' '){
+        i--;
+     }
+     while(i>=0 && s[i]!=' '){
+        i--;
+        count++;
+     }
+       return count;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
