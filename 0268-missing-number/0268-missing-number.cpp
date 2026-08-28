b745fc24666453cb9f32df3036be802c39767acb
Kanishk.s <skanishk135@gmail.com>
2026-08-28 22:58:00 +0530

    Time: 16 ms (9.74%), Space: 21.7 MB (93.92%) - LeetHub - Time: O(n log n), Space: O(1)

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=i){
+                return i;
+            }
+
+        }
+        return nums.size();
+      
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
