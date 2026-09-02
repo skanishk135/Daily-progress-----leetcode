e1807c281a81067e806b92ef95fe8d16cbda7922
Kanishk.s <skanishk135@gmail.com>
2026-09-02 22:07:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0011-reverse-nodes-in-k-group/0011-reverse-nodes-in-k-group.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
